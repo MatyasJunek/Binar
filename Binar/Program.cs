92813ffa5b73db462f212390d90cf1d8f4f18498
junek.matyas.2018 <junek.matyas.2018@UC313PC08>
2022-01-11 14:59:28 +0100

    Přidejte soubory projektu.

---
+++ Binar/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Binar
+{
+    class Program
+    {
+        public static int BinarFind(int find, int[] field, int indexFrom, int IndexTo)
+        {
+            int half = (indexFrom+IndexTo)/ 2;
+
+            if (indexFrom > IndexTo)
+                return -1;
+
+            if (find == field[half])
+                return half;
+
+            if (find < field[half])
+                return BinarFind(find,field, indexFrom, half - 1);
+
+            if (find > field[half])
+                return BinarFind(find, field, half+1, IndexTo);
+
+            return -1;
+            
+        }
+        static void Main(string[] args)
+        {
+            int count = 1;
+            int[] pole = new int[100000];
+            for (int i = 0; i < 100000; i++)
+            {
+                pole[i] = count;
+                count++;
+            }
+            Console.WriteLine(BinarFind(30, pole, 0,pole.Length)); 
+            
+        }
+    }
+}
